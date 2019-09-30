dac81f5af9770b7ac1c676d089ffc77bb8180ec8
Kyle Parisi <kyle.parisi@robustwealth.com>
2019-09-30 11:53:40 -0400

    Add deps

--- start.sh
+++ start.sh
@@ -5,5 +5,7 @@ NAME=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 # 10.242 is zerotier which will always be sequentially last, otherwise use the private
 # 10.0 address space.
 IP=$(ifconfig  | grep -E '10\.242|10\.0' | awk '{print $2}' | tail -n1)
+# Install dependencies
+mix deps.get
 # Start the app
 elixir --name $NAME@$IP --cookie test -S mix run --no-halt
