b39315ec62ef0d294e530aa1a227c5d26f01f320
Kyle Parisi <kyle.parisi@robustwealth.com>
2019-09-30 11:51:40 -0400

    Start script

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Random 8 character uuid
+NAME=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+# 10.242 is zerotier which will always be sequentially last, otherwise use the private
+# 10.0 address space.
+IP=$(ifconfig  | grep -E '10\.242|10\.0' | awk '{print $2}' | tail -n1)
+# Start the app
+elixir --name $NAME@$IP --cookie test -S mix run --no-halt
