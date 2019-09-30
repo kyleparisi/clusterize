fcedd90f6cd9a7529c8299b051f4ccc1c6a94d75
Kyle Parisi <kyle.parisi@robustwealth.com>
2019-09-30 12:08:01 -0400

    Refactor

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-# Random 8 character uuid
-NAME=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+NAME=$1
+if [ -z "$1" ]
+ then
+   echo "No arguments supplied"
+   # Random 8 character uuid
+   NAME=$(cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+fi
+echo "Name: $NAME"
 # 10.242 is zerotier which will always be sequentially last, otherwise use the private
 # 10.0 address space.
 IP=$(ifconfig  | grep -E '10\.242|10\.0' | awk '{print $2}' | tail -n1)
+echo "IP: $IP"
 # Install dependencies
 mix deps.get
 # Start the app
